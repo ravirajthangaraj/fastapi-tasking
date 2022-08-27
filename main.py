ecce06bf20ff042acebb31bb16088807255d4027
ravirajthangaraj <ravirajcse.2016@gmail.com>
2022-08-27 20:25:56 +0530

    added response model for root

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ application = FastAPI()
 application.include_router(users.router)
 
 
-@application.get('/')
+@application.get('/', response_model=dict)
 def root():
     # TODO: add title, version and description
     return {'message': 'Welcome to FastAPI Tasking'}
